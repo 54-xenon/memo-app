5f0186e205a8896cd0b60fc1a1fdc0d02e41d439
54-xenon <congas.rabbi_0b@icloud.com>
2025-03-13 20:24:14 +0900

    追加

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:memoapp/pages/home_page.dart';
 
 void main() {
   runApp( MyApp());
@@ -12,7 +13,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: "memo-app",
       debugShowCheckedModeBanner: false,
-      home: ,
+      home: HomePage(),
     );
   }
 }
